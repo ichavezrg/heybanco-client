e5c944a95f690c8c9073b09272af45aedfc7ba21
ichavezrg <ichavez@redgirasol.com>
2025-09-10 14:48:30 -0700

    Actualiza los espacios de nombres en la clase Caas para reflejar el nuevo prefijo del paquete "ichavezrg".

--- src/Caas.php
+++ src/Caas.php
@@ -2,9 +2,9 @@
 
 namespace Ichavezrg\HeyBancoClient;
 
-use Ichavez\HeyBancoClient\Caas\Agreement;
-use Ichavez\HeyBancoClient\Caas\Collection;
-use Ichavez\HeyBancoClient\Caas\User;
+use Ichavezrg\HeyBancoClient\Caas\Agreement;
+use Ichavezrg\HeyBancoClient\Caas\Collection;
+use Ichavezrg\HeyBancoClient\Caas\User;
 
 class Caas
 {
